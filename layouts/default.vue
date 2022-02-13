b3540c65fdee686c062c702a4dcddd405a1be8e2
Ronald Rupp <ronald.rupp@outlook.at>
2022-02-13 20:51:15 +0100

    added layout

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <Navigation></Navigation>
+    <Nuxt />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Navigation />
     <IndexHero />
   </div>
 </template>
