53959f995da6b2451aab0e983d0299f0dd5e34e7
Ronald Rupp <ronald.rupp@outlook.at>
2022-03-06 22:18:36 +0100

    added links to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,14 @@
 <template>
-  <footer class="container"><nuxt-link to="/" class="back-to-index">Ronald Rupp</nuxt-link></footer>
+  <footer class="container">
+    <section class="footer-left-section">
+      <nuxt-link to="/" class="back-to-index">Ronald Rupp</nuxt-link>
+      <span>Web Developer based in Vienna, Austria.</span>
+    </section>
+    <div class="other-links">
+      <a href="https://www.linkedin.com/in/ronaldrupp">LinkedIn</a>
+      <a href="mailto:ronald.rupp@outlook.at">E-Mail</a>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
@@ -14,15 +23,52 @@ export default Vue.extend({})
   margin: 0 auto;
   padding: 4em 1em;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
+  align-items: flex-start;
+  color: var(--gray-100);
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
+}
+.footer-left-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  span {
+    margin-top: 0.5em;
+    font-size: 0.75rem;
+  }
+}
+.other-links {
+  @media only screen and (max-width: 768px) {
+    margin-top: 1.5em;
+  }
+  a {
+    color: var(--gray-100);
+    font-weight: 600;
+    text-decoration: none;
+    &:not(:first-child) {
+      margin-left: 0.9em;
+    }
+    transition: 0.3s;
+    &:hover {
+      filter: brightness(1.5);
+    }
+  }
 }
 .back-to-index {
-  color: var(--gray-100);
   text-decoration: none;
   position: relative;
+  color: var(--gray-100);
+  transition: 0.3s;
   font-weight: 600;
-  &::after {
+  &:hover {
+    filter: brightness(1.5);
+  }
+  /* &::after {
     content: '';
     position: absolute;
     height: 1px;
@@ -39,6 +85,6 @@ export default Vue.extend({})
     &::after {
       transform: scaleX(1);
     }
-  }
+  } */
 }
 </style>
